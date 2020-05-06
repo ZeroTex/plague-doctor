673a6d6860311704c67b9a3bbe1e42ca7ac1ea5f
zerotex <zerotex@mail.com>
2020-05-06 18:27:27 -0500

    added some JS to fix side scroll issue, overflow-x: hidden did not suffice.

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,3 +167,15 @@ AOS.init();
   });
 
 })(jQuery);
+
+$(function() {
+
+  var $body = $(document);
+  $body.bind('scroll', function() {
+      // "Disable" the horizontal scroll.
+      if ($body.scrollLeft() !== 0) {
+          $body.scrollLeft(0);
+      }
+  });
+
+}); 
